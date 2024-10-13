7fcfd1d3da0ce9a72b90e5e666d387bc719b1175
U2D-8Bits <u2d.8bits@gmail.com>
2024-10-13 14:48:25 -0500

    Refactorización del app routing module para incluir las rutas de pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { NoFoundPageComponent } from './pages/no-found-page/no-found-page.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardPageComponent},
+  { path: '', redirectTo:'/dashboard', pathMatch: 'full' },
+  { path: '**', component: NoFoundPageComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
