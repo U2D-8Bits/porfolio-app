4b9685d473a1e0789bb2b1c604c73fe4d85ba8e1
U2D-8Bits <u2d.8bits@gmail.com>
2024-10-13 14:15:15 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HistoryPageComponent } from './pages/history-page/history-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistoryPageComponent,
+    DashboardPageComponent,
+    AboutPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
