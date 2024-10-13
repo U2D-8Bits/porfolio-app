ea9a9e308914d8b3c206dd6d670964776f6cd59d
U2D-8Bits <u2d.8bits@gmail.com>
2024-10-13 14:19:18 -0500

    Creación de los componentes Dashboard, About, & History

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HistoryPageComponent } from './pages/history-page/history-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardPageComponent,
+    AboutPageComponent,
+    HistoryPageComponent,
   ],
   imports: [
     BrowserModule,
