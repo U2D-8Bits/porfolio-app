3d4f2d807601b8beca74678a1c5ba851d71f9ddc
U2D-8Bits <u2d.8bits@gmail.com>
2024-10-13 14:17:44 -0500

    Creación del header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HistoryPageComponent } from './pages/history-page/history-page.component';
-import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HistoryPageComponent,
-    DashboardPageComponent,
-    AboutPageComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
